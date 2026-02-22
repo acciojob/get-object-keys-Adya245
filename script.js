d0f7904eac4099fcade0ed14b945600f16cf0410
Adya Bharatdwaj <adyabharatdwaj@gmail.com>
2026-02-22 17:00:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const student = {
+	name : "Richa",
+	age : 22,
+	city : "Delhi"
+}
+
+function getKeys(student)
+{
+	return Object.keys(student)
+}
+
+console.log(getKeys(student))
